da3abd94edcc027ee779637018cff61989bb3b56
Rasel Miah <rasel6041@gmail.com>
2022-01-25 04:04:38 +0100

    get sound dynamically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Controller from "./component/Controller";
 import DrumPad from "./component/DrumPad";
 
+
+
 function App() {
+  const play=(id)=>{
+    const audio=document.getElementById(id)
+    audio.currentTime=0;
+    audio.play()  
+  }
   return (
-    <div>
-        <div id="drum-machine">
-          <DrumPad />
+    <div id="drum-machine">
+        <div  id="display">
+          <DrumPad play={play}/>
         </div>
         <div>
           <Controller />
--- src/component/DrumPad.js
+++ src/component/DrumPad.js
@@ -1,23 +1,132 @@
 import React, { useState, useEffect } from "react";
+const firstSoundGroup = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
 
-export default function DrumPad() {
-  return <div id="display">
-     <div>
-      <button className='drum-pad' id="Q">Q</button>
-      <button className='drum-pad' id="W">W</button>
-      <button className='drum-pad' id="E">E</button>
-      </div>
-      <div>
-      <button className='drum-pad' id="A">A</button>
-      <button className='drum-pad' id="S">S</button>
-      <button className='drum-pad' id="D">D</button>
-      </div>
-      <div>
-      <button className='drum-pad' id="Z">Z</button>
-      <button className='drum-pad' id="X">X</button>
-      <button className='drum-pad' id="C">C</button>
-      </div>
+const secondSoundGroup = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  }
+];
 
-      
-  </div>;
+
+const DrumPadKey=( {play, sound: {keyTrigger, id, url} })=>{
+ return ( 
+        <button className="drum-pad" onClick={()=>play(id)}>
+            <audio className="clip" src={url} id={id}></audio>
+            {keyTrigger}
+        </button>
+ )}
+
+
+
+
+export default function DrumPad({ play }) {
+  
+  return firstSoundGroup.map((sound)=> <DrumPadKey play={play} sound={sound}/>)
+  
 }
